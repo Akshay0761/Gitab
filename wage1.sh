deda2236ce96ea0a859ac550617b98f62441514a
Akshay0761 <109942787+Akshay0761@users.noreply.github.com>
2022-07-25 23:21:05 +0530

    wage1.sh

---
+++ wage1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+
+Emp=1;
+randomcheck=$(( RANDOM%2 ));
+
+if [ $Emp -eq $randomcheck ];
+then
+        emprateperhour=20;
+        emphours=8;
+        salary=$(( $emphours*$emprateperhour ));
+else
+        salary=0;
+fi
